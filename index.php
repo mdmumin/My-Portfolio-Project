84dff7a482f2906ac8f1ec74588b6c6faef08a39
Md Momin <47780544+mdmumin@users.noreply.github.com>
2022-12-26 20:43:42 +0600

    Added index file

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <section>
+        <div>
+            <ul>
+                <li><a href="php-projrct#1-class-16/index.php">Assignment 01</a></li>
+            </ul>
+        </div>
+    </section>
+</body>
+
+</html>
